a03e79700b0b7a0200fb5562765ea21eac988a4f
LavrekhaLucy <lavrehaludmila@gmail.com>
2025-07-05 17:08:14 +0300

    hw/1

--- helpers/helper.js
+++
@@ -1,6 +0,0 @@
-const foo = () =>{
-    console.log("02/07/2025");
-}
-module.exports = {foo}
-
-
--- index.js
+++ index.js
@@ -1,44 +1,9 @@
 require ('./hello_world/hello_world');
-const {foo:helperFoo} = require ('./helpers/helper')
-const readline = require('node:readline/promises');
-const path = require('path');
 
-//
 const foo = async () =>{
-    console.log("02.07.2025");
-    helperFoo();
+
+    console.log("Hello from Node.js!");
 }
-foo();
-
-console.log("Hello from Node.js!");
-
-// console.log(__dirname);
-// console.log(__filename);
-// console.log(process.cwd());
-
-// const pathToFile = (__filename);
-
-// console.log(pathToFile);
-// console.log(path.dirname(pathToFile))
-// console.log(path.extname(pathToFile))
-// console.log(path.basename(pathToFile))
-// console.log(path.parse(pathToFile))
-// console.log(path.isAbsolute(pathToFile))
-// console.log(path.isAbsolute('USER\\IdeaProjects\\node\\index.js\n'))
-
-
-// Readline
-//
-// const rlInstance = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// })
-//
-// const name = await rlInstance.question('Name?');
-// console.log(`Your name is ${name}`);
-// process.exit(0);
-// }
-
 void foo();
 
 
