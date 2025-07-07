49aface8577365335134d2e44c37db6317cc1299
LavrekhaLucy <lavrehaludmila@gmail.com>
2025-07-07 09:37:49 +0300

    lesson/3

--- index.js
+++ index.js
@@ -1,44 +1,118 @@
-require ('./hello_world/hello_world');
-const {foo:helperFoo} = require ('./helpers/helper')
-const readline = require('node:readline/promises');
-const path = require('path');
-
-//
-const foo = async () =>{
-    console.log("02.07.2025");
-    helperFoo();
-}
-foo();
-
-console.log("Hello from Node.js!");
-
-// console.log(__dirname);
-// console.log(__filename);
-// console.log(process.cwd());
-
-// const pathToFile = (__filename);
-
-// console.log(pathToFile);
-// console.log(path.dirname(pathToFile))
-// console.log(path.extname(pathToFile))
-// console.log(path.basename(pathToFile))
-// console.log(path.parse(pathToFile))
-// console.log(path.isAbsolute(pathToFile))
-// console.log(path.isAbsolute('USER\\IdeaProjects\\node\\index.js\n'))
+const express = require('express');
+const app = express();
+const port = 3000;
 
 
-// Readline
-//
-// const rlInstance = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// })
-//
-// const name = await rlInstance.question('Name?');
-// console.log(`Your name is ${name}`);
-// process.exit(0);
-// }
+const users = [
+    {id:1,name: 'vera', email: 'halych@gmail.com', phone: '+380634521369'},
+    {id:2,name: 'petya', email: 'halych@gmail.com', phone: '+380634521369'},
+    {id:3,name: 'kolya',  email: 'halych@gmail.com', phone: '+380634521369'},
+    {id:4,name: 'olesya', email: 'halych@gmail.com', phone: '+380634521369'},
+    {id:5,name: 'max',  email: 'halych@gmail.com', phone: '+380634521369'},
+    {id:6,name: 'anya', email: 'halych@gmail.com', phone: '+380634521369'},
+    {id:7,name: 'oleg', email: 'halych@gmail.com', phone: '+380634521369'},
+    {id:8,name: 'andrey', email: 'halych@gmail.com', phone: '+380634521369'},
+    {id:9,name: 'masha', email: 'halych@gmail.com', phone: '+380634521369'},
+    {id:10,name: 'lucy', email: 'halych@gmail.com', phone: '+380634521369'},
 
-void foo();
+];
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// app.get('/users',  (req, res)=> {
+//     res.send ('Hello from Express!');
+// });
+
+
+app.get('/users',  (req, res)=> {
+    try{
+        res.send (users);
+    }
+    catch(err){
+        res.status(500).send('Error');
+    }
+
+});
+app.post('/users',  (req, res)=> {
+    try{
+        const {name,email,phone} = req.body;
+        // TODO validate data
+        const id = users[users.length - 1].id+1;
+        const newUser = {id,name,email,phone}
+        users.push(newUser);
+        res.status (201).send (newUser);
+    }
+    catch(err){
+        res.status(500).send('Error');
+    }
+
+});
+
+app.delete('/users/:userId',  (req, res)=> {
+    try{
+        const userId = Number(req.params.userId)
+        const userIndex =users.findIndex((user) => user.id === userId);
+        if (userIndex ===-1){
+            return res.status(404).send('User not found');
+        }
+        users.splice(userIndex, 1);
+        res.sendStatus(204);
+    }catch(err){
+        res.status(500).send('Error');
+    }
+
+})
+app.put ('/users/:userId',  (req, res)=> {
+    try{
+        const userId = Number(req.params.userId)
+        const userIndex =users.findIndex((user) => user.id === userId);
+        if (userIndex ===-1){
+            return res.status(404).send('User not found');
+        }
+        const {name,email,phone}=req.body;
+        // TODO validate data
+
+        users[userIndex].name = name;
+        users[userIndex].email = email;
+        users[userIndex].phone = phone;
+        res.sendStatus(201).send (users[userIndex]);
+
+    }catch(err){
+        res.status(500).send('Error');
+    }
+
+})
+
+
+
+app.get('/users/:userId',  (req, res)=> {
+    try{
+   const userId = Number(req.params.userId)
+   const user = users.find(user=>user.id === userId)
+    res.send (user);
+    }
+    catch(err){
+        res.status(500).send('Error');
+    }
+});
+
+app.post('/users',  (req, res)=> {
+    // console.log(req.body);
+    // console.log(req.query);
+    res.send ('Hello world!');
+});
+
+app.post('/users/:userId',  (req, res)=> {
+    console.log(req.body);
+    console.log(req.params);
+    console.log(req.query);
+    res.send ('Hello world!');
+});
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${ port }`);
+});
 
 
