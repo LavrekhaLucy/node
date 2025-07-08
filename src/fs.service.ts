6609b38a2c428f69db1ca1085946453f08b07637
LavrekhaLucy <lavrehaludmila@gmail.com>
2025-07-08 21:16:44 +0300

    lesson/4

--- src/fs.service.ts
+++ src/fs.service.ts
@@ -1,22 +1,23 @@
-// import * as fs from 'node:fs/promises';
-//
-// import path from 'node:path';
-//
-// const read = async () => {
-//     try{
-//         const pathToFile = path.join (process.cwd(), 'db.json');
-//         const data = await fs.readFile(pathToFile, 'utf8');
-//         return data? JSON.parse(data) : [];
-//     } catch(err) {
-//         console.log('write error', err.message);
-//     }
-// };
-// const write = async (users) => {
-//     try{
-//         const pathToFile = path.join (process.cwd(), 'db.json');
-//         await fs.writeFile(pathToFile, JSON.stringify(users), 'utf8');
-//     }catch(err) {
-//         console.log('write error', err.message);
-//     }
-// }
-// export {read, write}
+import * as fs from 'node:fs/promises';
+
+import path from 'node:path';
+import {IUser} from "./interfaces/user.interface.js";
+
+const read = async () => {
+    try{
+        const pathToFile = path.join (process.cwd(), 'db.json');
+        const data = await fs.readFile(pathToFile, 'utf8');
+        return data? JSON.parse(data) : [];
+    } catch(err) {
+        console.log('write error', err.message);
+    }
+};
+const write = async (users:IUser) => {
+    try{
+        const pathToFile = path.join (process.cwd(), 'db.json');
+        await fs.writeFile(pathToFile, JSON.stringify(users), 'utf8');
+    }catch(err) {
+        console.log('write error', err.message);
+    }
+}
+export {read, write}
