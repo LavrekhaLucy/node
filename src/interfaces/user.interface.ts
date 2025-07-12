2a96445324e2f7533487fc8fbc9f1038e9ddd587
LavrekhaLucy <lavrehaludmila@gmail.com>
2025-07-12 14:42:23 +0300

    lesson/5

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,9 +1,21 @@
+import {RoleEnum} from '../enums/enum';
+
 export interface IUser {
 
     id:number;
     name:string;
     age:number;
     email:string;
-    phone:string;
+    phone?:string;
+    role:RoleEnum;
+    isVerified:boolean;
+    isDeleted:boolean;
+    createdAt?:Date;
+    updatedAt?:Date;
+
+
+
+
+
 
 }
---
+++ src/models/model.ts
@@ -0,0 +1,20 @@
+import {model, Schema} from 'mongoose';
+import {IUser} from '../interfaces/user.interface';
+import {RoleEnum} from '../enums/enum';
+
+const UserSchema = new Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true, match: /^\S+@\S+\.\S+$/},
+    age: {type: Number, required: true},
+    phone: {type: Number, required: false},
+    role: {type: String, enum: RoleEnum, default: RoleEnum.User},
+    isVerified: {type: Boolean, default: false},
+    isDeleted: {type: Boolean, default: false},
+},
+{
+  timestamps: true,
+  versionKey: false,
+},
+
+);
+export const User = model<IUser>('users', UserSchema);
