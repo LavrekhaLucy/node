c419ec89075f0d3289013880b414a203e20cea71
LavrekhaLucy <lavrehaludmila@gmail.com>
2025-07-19 22:38:32 +0300

    hw/6

--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -50,7 +50,7 @@ export const signInSchema = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string()
         .pattern(new RegExp('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$'))
-        .required()
+        .optional()
         .messages({
             'string.pattern.base': 'Password must be at least 8 characters long and contain both letters and numbers',
         }),
